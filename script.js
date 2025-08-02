3b1782abaf7d53f92a80cae48c503a197e20316e
rebeccaqueiroz <152816132+rebeccaqueiroz@users.noreply.github.com>
2025-08-02 20:48:00 -0300

    Upload inicial do projeto

---
+++ script.js
@@ -0,0 +1,31 @@
+// Menu hamburger
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
